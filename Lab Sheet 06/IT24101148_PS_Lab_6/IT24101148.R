20790b14b56a3a883fb7ec2e49a13938f9dd672a
IT24101148 <IT24101148@my.sliit.lk>
2025-09-26 10:37:23 +0530

    Lab 6

---
+++ Lab Sheet 06/IT24101148_PS_Lab_6/IT24101148.R
@@ -0,0 +1,22 @@
+setwd("C:\\Users\\UsEr\\OneDrive\\Desktop\\PS\\IT24101148_PS_Lab_6")
+
+#Exercise 01
+
+#1)
+#Binomial Distribution
+#Here, Random veriable X has Binomial Distribution with n = 50 and p = 0.85 
+
+#2)
+pbinom(47,50,0.85,lower.tail = FALSE)
+
+#Exercise 02
+
+#1)
+#Customer calls per hour
+
+#2)
+#Poisson Distribution
+#Here, Random veriable X has Poisson Distribution with lamda = 12 
+
+#3)
+dpois(15,12)
